e931fa61f0b69a0f82415761acf1e1fbc1c86c86
Aledangelo <Alessandro.dangelo13.99@gmail.com>
2022-02-11 19:52:59 +0100

    Commit - logs

--- src/main/java/com/greenteam/ordineservice/controller/ordiniController.java
+++ src/main/java/com/greenteam/ordineservice/controller/ordiniController.java
@@ -3,16 +3,19 @@ package com.greenteam.ordineservice.controller;
 import com.greenteam.ordineservice.entity.Ordine;
 import com.greenteam.ordineservice.entity.Prodotto;
 import com.greenteam.ordineservice.entity.statoOrdine;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.greenteam.ordineservice.service.gestioneOrdini;
 import com.greenteam.ordineservice.service.gestioneRider;
 
 import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.LinkedList;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/ordini")
 public class ordiniController {
@@ -28,37 +31,63 @@ public class ordiniController {
     public Ordine saveOrdine(@RequestBody Ordine o) {return gestioneOrdini.saveOrdine(o); }
      */
 
-    @RolesAllowed("Admin")
-    @GetMapping("/drop/{id}")
-    public String deleteOrdine(@PathVariable("id") String id) { return gestioneOrdini.removeOrdine(id); }
+    private void logging(HttpServletRequest request) {
+        log.info("IP: " + request.getRemoteAddr() + " USER: " + request.getRemoteUser() + " REQUEST URL: " + request.getRequestURL() + " METHOD: " +request.getMethod());
+    }
 
-    @RolesAllowed("Rider")
+    @RolesAllowed("admin_role")
+    @GetMapping("/drop/{id}")
+    public String deleteOrdine(@PathVariable("id") String id, HttpServletRequest request) {
+        logging(request);
+        return gestioneOrdini.removeOrdine(id);
+    }
+
+    @RolesAllowed("rider_role")
     @GetMapping("/accept/{id_rider}/{id_ordine}")
-    public String accettaOrdine(@PathVariable("id_rider") String id_rider, @PathVariable("id_ordine") String id_ordine) {
+    public String accettaOrdine(@PathVariable("id_rider") String id_rider, @PathVariable("id_ordine") String id_ordine, HttpServletRequest request) {
+        logging(request);
         return gestioneRider.accettaOrdine(id_rider, id_ordine);
     }
 
-    @RolesAllowed("Rider")
+    @RolesAllowed("rider_role")
     @GetMapping("/complete/{id_rider}")
-    public String chiudiOrdine(@PathVariable("id_rider") String id_rider) { return gestioneRider.chiudiOrdine(id_rider); }
+    public String chiudiOrdine(@PathVariable("id_rider") String id_rider, HttpServletRequest request) {
+        logging(request);
+        return gestioneRider.chiudiOrdine(id_rider);
+    }
 
-    @RolesAllowed("Rider")
+    @RolesAllowed("rider_role")
     @GetMapping("/cancel/{id}")
-    public String cancellaOrdine(@PathVariable("id") String id) { return gestioneOrdini.updateStato(id, statoOrdine.ANNULLATO); }
+    public String cancellaOrdine(@PathVariable("id") String id, HttpServletRequest request) {
+        logging(request);
+        return gestioneOrdini.updateStato(id, statoOrdine.ANNULLATO);
+    }
 
-    @RolesAllowed("Admin")
+    @RolesAllowed("admin_role")
     @GetMapping("/suspend/{id}")
-    public String sospendiOrdine(@PathVariable("id") String id) { return gestioneRider.sospendiOrdine(id); }
+    public String sospendiOrdine(@PathVariable("id") String id, HttpServletRequest request) {
+        logging(request);
+        return gestioneRider.sospendiOrdine(id);
+    }
 
-    @RolesAllowed({"Admin", "Rider"})
+    @RolesAllowed({"admin_role", "rider_role"})
     @GetMapping("/get/{id}")
-    public Ordine getOrdine(@PathVariable("id") String id) { return gestioneOrdini.getOrdine(id); }
+    public Ordine getOrdine(@PathVariable("id") String id, HttpServletRequest request) {
+        logging(request);
+        return gestioneOrdini.getOrdine(id);
+    }
 
     @RolesAllowed({"admin_role", "rider_role"})
     @GetMapping("/all")
-    public List<Ordine> getAll() { return gestioneOrdini.getAll(); }
+    public List<Ordine> getAll(HttpServletRequest request) {
+        logging(request);
+        return gestioneOrdini.getAll();
+    }
 
-    @RolesAllowed("User")
+    @RolesAllowed({"user_role","admin_role"})
     @PostMapping("/createOrder")
-    public <Any> Any createOrder(@Valid @RequestBody LinkedList<Prodotto> l) { return gestioneOrdini.addToOrder(l); }
+    public <Any> Any createOrder(@Valid @RequestBody LinkedList<Prodotto> l, HttpServletRequest request) {
+        logging(request);
+        return gestioneOrdini.addToOrder(l);
+    }
 }
